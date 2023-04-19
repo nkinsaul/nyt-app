0b0e801a409853ebe54dd38a5869af65c03c96e6
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-04-19 11:04:48 -0700

    Add fetch call for news data

--- src/Components/Stories/Stories.js
+++ src/Components/Stories/Stories.js
@@ -1,6 +1,29 @@
-import React from "react";
+import {React, useEffect, useState} from "react";
 
 const Stories = () => {
+  const [news, setNews] = useState();
+  const [error, setError] = useState();
+
+  const getStories = () => {
+    const url = `https://api.nytimes.com/svc/topstories/v2/us.json?api-key=dUHXsUzOQoAMZs5klSRIGW2cU4Bawp9g`
+    return fetch(url)
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw new Error(res)
+      }
+    })
+    .catch(err => {
+      setError(err)
+    })
+  }
+
+  useEffect(() => {
+    getStories()
+    .then (data => setNews(data))  
+  },[])
+
   return (
     <div>
       Stories
