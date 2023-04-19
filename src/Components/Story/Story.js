7d0e31bd9f80a521d2e1286eed96dbec2166d006
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-04-19 12:11:43 -0700

    Add basic styling to story grid

--- src/Components/Story/Story.js
+++ src/Components/Story/Story.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Story.css'
 
 const Story = ({title, abstract, pictures}) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <img src={pictures[1].url}/>
+    <div className="story">
+      <h1 className="title">{title}</h1>
+      <img className="thumbnail" src={pictures[1].url}/>
     </div>
   )
 }
