4bd93a8319a61237249d1b5733df86711169c53c
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-04-19 10:27:23 -0700

    Initial setup

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from '../Header/Header'
+import Stories from '../Stories/Stories'
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Stories />
+    </main>
+  );
+}
+
+export default App;
