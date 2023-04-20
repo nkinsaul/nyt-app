97fc054d5824497bed6d70c0b7ec90404925392d
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-04-20 10:19:28 -0700

    Add link to full article

--- src/Components/StoryView/StoryView.js
+++ src/Components/StoryView/StoryView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 import './StoryView.css'
 
 
@@ -12,6 +13,9 @@ const StoryView = ({news}) => {
       <div className="infoCont">
         <h1 className="SVtitle">{news[id].title}</h1>
         <h2 className="SVabstract">{news[id].abstract}</h2>
+        <Link to={`${news[id].url}`}>
+          <h3>Full NYT Article</h3>
+        </Link>
       </div>
       <div className="imgCont">
         <img className="SVimg" src={news[id].multimedia[1].url}/>
