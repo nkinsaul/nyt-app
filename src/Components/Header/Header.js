5e44a6c1a66979e986555dc8aa4007a23340bd71
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-04-21 08:46:52 -0700

    Add link to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Header.css'
 
 const Header = () => {
   return (
     <header>
-      New York Times
+      <Link to='/'>
+          New York Times
+      </Link>
     </header>
   )
 }
